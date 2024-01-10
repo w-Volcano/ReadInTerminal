042bde6ee8d28e0f868b0fc97c7e29f5790db351
w-Volcano <1317832781@qq.com>
2024-01-10 08:56:58 +0800

    feat: 输出增加部分颜色

--- startRead.js
+++ startRead.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 const jschardet = require('jschardet');
 const iconv = require('iconv-lite');
 const readlineSync = require('readline-sync');
+const clc = require('cli-color');
+readlineSync.setEncoding('utf8')
 const r1 = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -20,7 +22,7 @@ try {
     let tmp = fs.readFileSync(path.join(__dirname,'./settings.json'), 'utf-8')
     settings = JSON.parse(tmp)
 } catch (error) {
-    console.log('读取配置文件失败，请检查配置文件存在或格式');
+    console.log(clc.red('读取配置文件失败，请检查配置文件存在或格式'));
     process.exit(0)
 }
 let bookList = settings.bookList
@@ -36,19 +38,19 @@ try {
         return path.join(bookList, fileName)
     }).filter(isFile)
 } catch (error) {
-    console.log('读取书库失败，请在setting.json中检查配置路径');
+    console.log(clc.red('读取书库失败，请在setting.json中检查配置路径'));
     process.exit(0)
 }
 
-console.log(`当前书库共有${books.length}本书，请选择：`)
+console.log(clc.underline(`当前书库共有${books.length}本书，请选择：`))
 for (let i = 0; i < books.length; i++) {
-    console.log(`${i + 1} —— ${getBookName(books[i])}`)
+    console.log(clc.blue.bold(`${i + 1} —— ${getBookName(books[i])}`))
 }
 let bookData = ''
 let bookDataLines = []
 r1.question(`选择你要阅读的书籍（输入书名前的数字）：`, num => {
     if(num<1||num>books.length){
-        console.log('非法输入，退出程序...');
+        console.log(clc.red('非法输入，退出程序...'));
         r1.close()
         return
     }
@@ -57,7 +59,7 @@ r1.question(`选择你要阅读的书籍（输入书名前的数字）：`, num 
     const detectedEncoding = jschardet.detect(buffer);
     const detectedCharset = detectedEncoding && detectedEncoding.encoding;
     if(!detectedCharset){
-        console.log("未检测出目标txt的编码，程序退出")
+        console.log(clc.red("未检测出目标txt的编码，程序退出"))
         r1.close()
         return
     }
@@ -77,10 +79,10 @@ r1.question(`选择你要阅读的书籍（输入书名前的数字）：`, num 
             }
             if(his.length!==bookDataLines.length){
                 his.currentPage = parseInt((his.currentPage/his.length)*bookDataLines.length)
-                console.log('检测到页面大小被修改，更新当前页码为'+his.currentPage);
+                console.log(clc.bgWhite.black('检测到页面大小被修改，更新当前页码为'+his.currentPage));
                 his.length = bookDataLines.length
             }
-            console.log(`加载历史记录完毕，现在《${getBookName(books[num - 1])}》第${his.currentPage}页，页面大小${his.pageSize}字，共${bookDataLines.length}页`);
+            console.log(`加载历史记录完毕，现在《${clc.blue.bold(getBookName(books[num - 1]))}》第${clc.red.bold(his.currentPage)}页，页面大小${clc.yellow.bold(his.pageSize)}字，共${clc.green.bold(bookDataLines.length)}页`);
         } else {
             for (let i = 0; i < bookData.length / settings.pageSize; i++) {
                 bookDataLines.push(bookData.substr(i * settings.pageSize, settings.pageSize))
@@ -92,7 +94,7 @@ r1.question(`选择你要阅读的书籍（输入书名前的数字）：`, num 
                 length:bookDataLines.length
             })
             his = settings.history[settings.history.length-1]
-            console.log(`未找到历史记录，已创建，现在《${getBookName(books[num - 1])}》第${his.currentPage}页，页面大小${his.pageSize}字，共${bookDataLines.length}页`);
+            console.log(`未找到历史记录，已创建，现在《${clc.blue.bold(getBookName(books[num - 1]))}》第${clc.red.bold(his.currentPage)}页，页面大小${clc.yellow.bold(his.pageSize)}字，共${clc.green.bold(bookDataLines.length)}页`);
         }
         // console.log(bookDataLines[bookDataLines.length-1]);
     })
@@ -102,13 +104,10 @@ r1.question(`选择你要阅读的书籍（输入书名前的数字）：`, num 
         console.log(bookDataLines[his.currentPage - 1])
         while (flag) {
             let input
-            if(settings.showPercent){
-                console.log(`${((his.currentPage-1)/bookDataLines.length*100).toFixed(2)+'%'}；<${keyMap.PageUp}>：上一页；<${keyMap.PageDown}>：下一页；<${keyMap.Jump}>：跳转`);
-                input = readlineSync.question(``)
-            }else{
-                console.log(`${his.currentPage}/${bookDataLines.length}；<${keyMap.PageUp}>：上一页；<${keyMap.PageDown}>：下一页；<${keyMap.Jump}>：跳转`);
-                input = readlineSync.question(``)
-            }
+            const percent = ((his.currentPage-1)/bookDataLines.length*100).toFixed(2)+'%'
+            const pageProgress = `${his.currentPage}/${bookDataLines.length}`
+            console.log(`${settings.showPercent?clc.blue.bold(percent):""}；${clc.blue.bold(pageProgress)}；<${clc.red.bold(keyMap.PageUp)}>：上一页；<${clc.yellow.bold(keyMap.PageDown)}>：下一页；<${clc.green.bold(keyMap.Jump)}>：跳转`);
+            input = readlineSync.question(``)
             let originPage = his.currentPage
             if (input.toLowerCase() == keyMap.PageUp) {
                 his.currentPage > 1 ? his.currentPage -= 1 : console.log('已经是第一页')
@@ -119,12 +118,12 @@ r1.question(`选择你要阅读的书籍（输入书名前的数字）：`, num 
                 if(page>=1&&page<=bookDataLines.length){
                     his.currentPage = parseInt(page)
                 }else{
-                    console.log('非法输入，退出程序');
+                    console.log(clc.red('非法输入，退出程序'));
                     flag = false
                 }
             } else {
                 console.clear()
-                console.log('非法输入，退出程序');
+                console.log(clc.red('非法输入，退出程序'));
                 fs.writeFileSync(path.join(__dirname,'./settings.json'), JSON.stringify(settings,null,2), 'utf-8')
                 flag = false
             }
